6f0e4e34286308a983c1b7fd2f2cda7f0c02dacc
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-10-21 02:28:06 +0300

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList myList= new MyList();
+        myList.Add(0);
+        myList.Add(1);
+        myList.Add(2);
+        myList.Insert(0, -1);
+        myList.Remove(2);
+        myList.RemoveAt(2);
+        
+        Console.WriteLine(myList.ToString());
+    }
+}
