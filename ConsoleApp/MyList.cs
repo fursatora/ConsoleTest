2e95fbb702bf119ee7807fa86621ce248d95faa3
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-10-22 13:29:38 +0300

    HomeWork
    Functions for Array

--- ConsoleApp/MyList.cs
+++ ConsoleApp/MyList.cs
@@ -9,15 +9,117 @@ public class MyList
         get { return _count; }
     }
 
-    public void Add(int item) {}
+    public void Add(int item) 
+    {
+         if (_count >= _array.Length)
+        {
+            int[] _new_array = new int[_array.Length + 1];
+            
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _new_array[i] = _array[i];
+            }
+            _array = _new_array;
+        }
 
-    public void Remove(int item) {}
+        _array[_count] = item;
+        _count++;  
+    }
 
-    public void RemoveAt(int index) {}
+    public void Remove(int item) {
+        int _remove_index = -1;
 
-    public void Insert(int index, int item) {}
+        for (int i = 0; i < _count; i++)
+        {
+            if (_array[i] == item)
+            {
+                _remove_index = i;
+                break;  
+            }
+        }
 
-    public void Clear() {}
+        if (_remove_index == -1)
+        {
+            return;
+        }
+
+        for (int i = _remove_index; i < _count - 1; i++)
+        {
+            _array[i] = _array[i + 1];
+        }
+            _count--;
+
+        
+
+        int[] _new_array = new int[_array.Length - 1];
+        for (int i = 0; i < _new_array.Length; i++)
+        {
+            _new_array[i] = _array[i];
+        }
+
+        _array = _new_array;
+        
+    }
+        
+
+    public void RemoveAt(int index)
+    {
+       
+        for (int i = index; i < _count - 1; i++)
+        {
+            _array[i] = _array[i + 1];
+        }
+
+        int[] _new_array = new int[_array.Length - 1];
+        for (int i = 0; i < _new_array.Length; i++)
+        {
+            _new_array[i] = _array[i];
+        }
+
+        _array = _new_array;
+        _count--;
+
+    }
+
+    public void Insert(int index, int item)
+    {   if (index<0 || index>_array.Length)
+        {
+            Console.WriteLine("Ошибка введения индекса");
+            return;
+        }
+
+        if (index==_array.Length){
+            Add(item);
+            return;
+        }
+        if (index<_array.Length)
+        {
+            int[] _new_array = new int[_array.Length + 1];
+            for (int i=0; i<index; i++)
+            {
+                _new_array[i]=_array[i];
+            }
+
+            _count++;
+            _new_array[index]=item;
+
+            for (int i=index; i<_array.Length; i++)
+            {
+                _new_array[i+1]=_array[i];
+            }
+
+            _array = _new_array;
+        }
+    }
+
+
+    public void Clear() {
+        for (int i=0; i<_array.Length+1; i++){
+            RemoveAt(i);
+            _count--;
+        }
+        return;
+    }
 
     public string ToString() 
     { 
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,9 +6,13 @@
         myList.Add(0);
         myList.Add(1);
         myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
+
         myList.Insert(0, -1);
         myList.Remove(2);
         myList.RemoveAt(2);
+        myList.Clear();
         
         Console.WriteLine(myList.ToString());
     }
